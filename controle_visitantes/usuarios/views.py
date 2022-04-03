88fc3bd9d645d8895c0e63c34f184fc8bc9121d1
Bruno Oliveira <bruoli3@gmail.com>
2022-04-03 13:29:46 -0300

    Dashboard controle de visitantes

--- controle_visitantes/usuarios/views.py
+++ controle_visitantes/usuarios/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    context = {
+        "nome_pagina": "Início da dashboard",
+    }
+    return render(request, "index.html", context)
