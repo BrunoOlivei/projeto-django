de3aaf94c98aaf08f3bfcfe8b63ef4a034942df1
Bruno Oliveira <bruoli3@gmail.com>
2022-04-03 18:27:24 -0300

    Registra novos visitantes com a informação do porteiro que está logado no sistema Redireciona para index após cadastro de novo visitante.

--- controle_visitantes/visitantes/forms.py
+++ controle_visitantes/visitantes/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from visitantes.models import Visitante
 
+
 class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = [
+            "nome_completo", "cpf", "data_nascimento",
+            "numero_casa", "placa_veiculo"
+        ]
