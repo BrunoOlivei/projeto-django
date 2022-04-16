7ce37b096ba1e643b0b629c3ff0373db832e9ba0
Bruno Oliveira <bruoli3@gmail.com>
2022-04-16 17:23:46 -0300

    Query com quantidade de visitantes no mês corrente e ordenação da lista de visitantes do mais recente (horario de chegada)

--- controle_visitantes/apps/dashboard/views.py
+++ controle_visitantes/apps/dashboard/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from visitantes.models import Visitante
+from django.utils import timezone
 
 
 def index(request):
-    todos_visitantes = Visitante.objects.all()
+    todos_visitantes = Visitante.objects.order_by(
+        "-horario_chegada"
+    )
 
     visitantes_aguardando = todos_visitantes.filter(status='AGUARDANDO')
     visitantes_em_visita = todos_visitantes.filter(status='EM_VISITA')
     visitantes_finalizados = todos_visitantes.filter(status='FINALIZADO')
+
+    hora_atual = timezone.now()
+    mes_atual = hora_atual.month
+
+    visitantes_mes = todos_visitantes.filter(horario_chegada__month=mes_atual)
 
     context = {
         "nome_pagina": "Início da dashboard",
@@ -15,5 +23,6 @@ def index(request):
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizados": visitantes_finalizados.count(),
+        "visitantes_mes": visitantes_mes.count(),
     }
     return render(request, "index.html", context)
